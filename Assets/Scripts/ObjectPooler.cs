93e09cf03a40a0f293c12351830ef2449d857372
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-29 22:15:01 +0100

    initial commit

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int poolSize = 5;
+    private List<GameObject> _pool;
+
+
+    void Start()
+    {
+        // create pool
+        _pool = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            CreateNewGameObject();
+        }
+    }
+
+    private GameObject CreateNewGameObject()
+    {
+        GameObject obj = Instantiate(prefab, transform);
+        obj.SetActive(false);
+        _pool.Add(obj);
+
+        return obj;
+    }
+
+    public GameObject GetPooledObject()
+    {
+        foreach (GameObject obj in _pool)
+        {
+            if (!obj.activeSelf)
+            {
+                return obj;
+            }
+        }
+
+        return CreateNewGameObject(); 
+    }
+}
