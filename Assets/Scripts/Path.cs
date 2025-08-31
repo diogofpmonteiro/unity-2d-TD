12d2eed8fb30142b69e6c243e831c8a2778a2244
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2025-08-31 10:07:06 +0100

    fix Handles runtime bug, remove if not in unity editor

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,4 +1,6 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 public class Path : MonoBehaviour
@@ -16,10 +18,12 @@ public class Path : MonoBehaviour
         {
             for (int i = 0; i < Waypoints.Length; i++)
             {
+                #if UNITY_EDITOR
                 GUIStyle style = new GUIStyle();
                 style.normal.textColor = Color.white;
                 style.alignment = TextAnchor.MiddleCenter;
                 Handles.Label(Waypoints[i].transform.position + Vector3.up * 0.7f, Waypoints[i].name, style);
+                #endif
 
                 if (i < Waypoints.Length - 1)
                 {
